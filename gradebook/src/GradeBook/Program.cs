96e524c3d337a1ce9b8f4b6645df58eb4f30f14b
Gurunath <guruthakur16@gmail.com>
2020-10-12 22:09:23 +0530

    Introducing C# and .NET

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() { 12.7, 7.5, 23.5, 6.4 };
+            grades.Add(56.3);
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The Average grade id {result:N1}");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!!");
+            }
+        }
+    }
+}
